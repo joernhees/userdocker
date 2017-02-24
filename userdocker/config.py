4a3c81f8a6a659532bf05985aa9537c9101d856a
Joern Hees <dev@joernhees.de>
2017-02-24 17:53:29 +0100

    volume mounts split into always, available and default, probe mounts, allowed image regexps, pull restrictions

--- userdocker/config.py
+++ userdocker/config.py
@@ -4,13 +4,53 @@ EXECUTORS = {
     'docker': '/usr/bin/docker',
     'nvidia-docker': '/usr/bin/nvidia-docker',
 }
-DEFAULT_EXECUTOR = 'nvidia-docker'
+DEFAULT_EXECUTOR = 'docker'
 
-VOLUME_MOUNTS = []
-MOUNT_USER_HOME = True
 
+# Volume mounts:
+# - VOLUME_MOUNTS_ALWAYS will be mounted whether the user wants it or not
+# - VOLUME_MOUNTS_AVAILABLE will only be mounted if the user explicitly
+#   specifies them
+# - VOLUME_MOUNTS_DEFAULT will be added unless the user specifies the
+#   --no-default-mounts option
+# - The user can mount any of the above explicitly with "-v", redundancy is ok
+# - You (admin) can use "{USER}" and "{HOME}" vars in mount specs
+# - You (admin) can specify whatever usually comes after the "-v" arg in
+#   "host_path:container_path:flags" form. If you do not specify a target, the
+#   user may select one, which is potentially unsafe. If you don't specify a
+#   flag, the user can append a "ro" to guard herself (even for
+#   VOLUME_MOUNTS_ALWAYS).
+# Example:
+# VOLUME_MOUNTS_DEFAULT = ["/netscratch:/netscratch", "/data:/input:ro"]
+VOLUME_MOUNTS_ALWAYS = []
+VOLUME_MOUNTS_AVAILABLE = []
+VOLUME_MOUNTS_DEFAULT = [
+    # default mount user home
+    "{HOME}:{HOME}",
+]
+
+# This setting issues a listdir for used host dirs in mounts.
+# Useful for server-side auto-mounts.
+PROBE_USED_MOUNTS = True
+
+
+# User is allowed to run an image if any of the following regexps match it
+ALLOWED_IMAGE_REGEXPS = [
+    '.*',
+]
+
+# Normally docker run automatically pulls images that aren't available locally.
+# This possible values here are ['default', 'never', 'always']
+# 'never' will restrict to locally available images.
+RUN_PULL = 'default'
+
+
+# If set, the user will run with his uid and gid in the container.
+# Changing this to false is probably a really bad idea, especially when combined
+# with any mounts.
 USER_IN_CONTAINER = True
 
+# Environment vars to set for the container
 ENV_VARS = []
 ENV_VARS_EXT = {
     'nvidia-docker': [
--- userdocker/userdocker.py
+++ userdocker/userdocker.py
@@ -1,24 +1,43 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
+"""
+This is a wrapper to allow restricted access to the docker command to users.
+
+Feedback welcome:
+https://github.com/joernhees/userdocker
+"""
+
 import argparse
 import os
 import pwd
+import re
 import subprocess
 import sys
 
 from config import *
 
 
+class UserDockerException(Exception):
+    pass
+
+
 def parse_args():
     parser = argparse.ArgumentParser(
-        description='allows restricted access to docker for users',
+        description=__doc__.strip(),
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
-    parser.add_argument(
+    debug_group = parser.add_mutually_exclusive_group()
+    debug_group.add_argument(
         "--quiet",
         help="silences output of invoked docker command",
+        action="store_true",
+    )
+
+    debug_group.add_argument(
+        "--debug",
+        help="even more output for invoked docker command",
         action="store_true",
     )
 
@@ -28,50 +47,139 @@ def parse_args():
         action="store_true",
     )
 
+    # will be replaced with its dict value, see end of function
     parser.add_argument(
         "--executor",
         help="prints the invoked docker commandline",
-        action="store",
         default=DEFAULT_EXECUTOR,
-        type=str,
         choices=EXECUTORS,
     )
 
-    parser.add_argument(
-        "image",
-        help="the image to run",
-        action="store",
-        type=str,
+    # individual commands will be sub-parsers
+    subparsers = parser.add_subparsers(dest='cmd')
+    subparsers.required = True
+
+    ############################################################################
+    # parser for docker run
+    ############################################################################
+    parser_run = subparsers.add_parser(
+        'run',
+        help='lets a user run a "docker run"',
+    )
+    parser_run.set_defaults(prepare_commandline=prepare_commandline_run)
+
+    parser_run.add_argument(
+        "--no-default-mounts",
+        help="does not automatically add default mounts",
+        action="store_true",
     )
 
-    parser.add_argument(
+    parser_run.add_argument(
+        "-v", "--volume",
+        help="user specified volume mounts (can be given multiple times)",
+        action="append",
+        dest='volumes',
+        default=[],
+    )
+
+    parser_run.add_argument(
+        "image",
+        help="the image to run",
+    )
+
+    parser_run.add_argument(
         "image_args",
         help="arguments passed to the image",
-        action="store",
-        type=str,
         nargs=argparse.REMAINDER
     )
 
-    return parser.parse_args()
+    ############################################################################
+    # parser for docker ps
+    ############################################################################
+    parser_ps = subparsers.add_parser(
+        'ps',
+        help='lets a user run a "docker ps"',
+    )
+    parser_ps.set_defaults(prepare_commandline=prepare_commandline_ps)
 
 
-def build_docker_commandline(args):
+    args = parser.parse_args()
+    args.executor = EXECUTORS[args.executor]
+    return args
+
+
+def expand_mounts(mounts, **kwds):
+    return [m.format(**kwds) for m in mounts]
+
+
+def prepare_commandline_run(args):
     uid = os.getuid()
     uid = int(os.getenv('SUDO_UID', uid))
     gid = os.getgid()
     gid = int(os.getenv('SUDO_GID', gid))
-    # user_name = pwd.getpwuid(uid)[0]
+    user_name = pwd.getpwuid(uid)[0]
     user_home = pwd.getpwuid(uid)[5]
+    mt_args = {'USERNAME': user_name, 'HOME': user_home}
 
     cmd = [args.executor]
     cmd += ['run']
     cmd += ARGS
 
-    for volume_mount in VOLUME_MOUNTS:
-        cmd += ['-v', volume_mount]
+    mounts = []
+    mounts_always = expand_mounts(VOLUME_MOUNTS_ALWAYS, **mt_args)
+    mounts_default = expand_mounts(VOLUME_MOUNTS_DEFAULT, **mt_args)
+    mounts_available = mounts_always + mounts_default + \
+        expand_mounts(VOLUME_MOUNTS_AVAILABLE, **mt_args)
 
-    if MOUNT_USER_HOME:
-        cmd += ['-v', '%s:%s' % (user_home, user_home)]
+    mounts += mounts_always
+
+    if not args.no_default_mounts:
+        mounts += expand_mounts(VOLUME_MOUNTS_DEFAULT, **mt_args)
+
+    for user_mount in args.volumes:
+        if user_mount in mounts:
+            continue
+        if user_mount in mounts_available:
+            mounts += [user_mount]
+            continue
+
+        # literal matches didn't work, allow potential unspecified
+        # container_path mounts
+        host_path = user_mount.split(':')[0]
+        if host_path in mounts:
+            continue
+        if user_mount in mounts_available:
+            mounts += [user_mount]
+            continue
+
+        # check if the user appended a 'ro' flag
+        if len(user_mount.split(':')) == 3:
+            host_path, container_path, flag = user_mount.split(':')
+            if flag == 'ro':
+                st = ':'.join([host_path, container_path])
+                if st in mounts:
+                    # upgrade mount to include ro flag
+                    idx = mounts.index(st)
+                    mounts[idx] = user_mount
+                    continue
+                if st in mounts_available:
+                    mounts += [user_mount]
+
+        raise UserDockerException(
+            "ERROR: given mount not allowed: %s" % user_mount
+        )
+
+    mount_host_paths = [m.split(':')[0] for m in mounts]
+    for ms in mount_host_paths:
+        if not os.path.exists(ms):
+            raise UserDockerException(
+                "ERROR: mount can't be found: %s" % ms
+            )
+        if PROBE_USED_MOUNTS and os.path.isdir(ms):
+            os.listdir(ms)
+
+    for mount in mounts:
+        cmd += ['-v', mount]
 
     for env_var in ENV_VARS + ENV_VARS_EXT.get(args.executor, []):
         cmd += ['-e', env_var]
@@ -80,23 +188,83 @@ def build_docker_commandline(args):
         cmd += ['-u', '%d:%d' % (uid, gid)]
 
     cmd.append('--')
-    cmd.append(args.image)
+
+    img = args.image
+    if ALLOWED_IMAGE_REGEXPS:
+        for air in ALLOWED_IMAGE_REGEXPS:
+            if re.match(air, img):
+                break
+        else:
+            raise UserDockerException(
+                "ERROR: image not in allowed images: %s" % img
+            )
+
+    # pull image?
+    if RUN_PULL == 'default':
+        # just let `docker run` do its thing
+        pass
+    elif RUN_PULL == 'always':
+        # pull image
+        exec_cmd([args.executor, 'pull', img], args)
+    elif RUN_PULL == 'never':
+        # check if image is available locally
+        if not subprocess.check_output([args.executor, 'images', '-q', img]):
+            raise UserDockerException(
+                "ERROR: you can only use locally available images, but %s could"
+                " not be found locally" % img
+            )
+    else:
+        raise UserDockerException(
+            "ERROR: RUN_PULL config variable not expected range, contact admin"
+        )
+
+    cmd.append(img)
     cmd.extend(args.image_args)
 
     return cmd
 
 
-def main():
-    args = parse_args()
-    cmd = build_docker_commandline(args)
+def prepare_commandline_ps(args):
+    cmd = [args.executor, 'ps', '-a']
+    return cmd
 
+
+def exec_cmd(cmd, args):
     if not args.quiet:
-        print(cmd)
+        print(
+            'would execute' if args.dry_run else 'executing',
+            'command:',
+            ' '.join(cmd)
+        )
+    if args.debug:
+        print('internal repr:', cmd)
 
-    if not args.dry_run:
-        sys.exit(subprocess.check_call(
+    if args.dry_run:
+        return 0
+
+    if not os.path.exists(cmd[0]):
+        raise UserDockerException(
+            "ERROR: can't find docker executable: %s" % cmd[0]
+        )
+
+    try:
+        ret = subprocess.check_call(
             cmd,
-        ))
+        )
+    except subprocess.CalledProcessError as e:
+        ret = e.returncode
+        sys.exit(ret)
+    return ret
+
+
+def main():
+    try:
+        args = parse_args()
+        cmd = args.prepare_commandline(args)
+        exec_cmd(cmd, args)
+    except UserDockerException as e:
+        print(e, file=sys.stderr)
+        sys.exit(1)
 
 
 if __name__ == '__main__':
