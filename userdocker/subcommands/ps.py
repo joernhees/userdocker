89422442e2d8044f8ea9d126ac73147a222224ec
Joern Hees <dev@joernhees.de>
2017-03-26 19:55:58 +0200

    userdocker ps can now show --gpu-reservations or --gpu-free

---
+++ userdocker/subcommands/ps.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from ..helpers.cmd import init_cmd
+from ..helpers.nvidia import nvidia_get_available_gpus
+from ..helpers.nvidia import nvidia_get_gpus_used_by_containers
+from ..helpers.parser import init_subcommand_parser
+
+
+def parser_ps(parser):
+    sub_parser = init_subcommand_parser(parser, 'ps')
+
+    arg_group = sub_parser.add_mutually_exclusive_group()
+    arg_group.add_argument(
+        "--gpu-reservations",
+        help="show userdocker reserved GPUs",
+        action="store_true",
+    )
+
+    arg_group.add_argument(
+        "--gpu-free",
+        help="show allowed free GPUs",
+        action="store_true",
+    )
+
+
+def prepare_commandline_ps(args):
+    if not args.gpu_reservations and not args.gpu_free:
+        return init_cmd(args)
+
+    gpus_used = nvidia_get_gpus_used_by_containers(args.executor_path)
+    if args.gpu_reservations:
+        for i, (container, container_name, user) in sorted(gpus_used.items()):
+            print("\t".join((i, container, container_name, user)))
+    elif args.gpu_free:
+        available_gpus = nvidia_get_available_gpus(args.executor_path)
+        for gpu in available_gpus:
+            print(gpu)
